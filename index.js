801d71bd8f902258d382a937e04622477be35866
jennimardegan <jennifer_mardegan@hotmail.com>
2019-12-05 10:00:42 -0300

    Atualizacao programa Tone Analyzer

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+var ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
+var text = "In my younger and more vulnerable years my father gave me some advice that I’ve been" 
++ "turning over in my mind ever since. \“Whenever you feel like criticizing any one,\” he told me," 
++ "\“just remember that all the people in this world haven’t had the advantages that you’ve had.\"";
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: 'dD5roo63qg6dmmoCN0JA2RGFcxk9P_lyNBMVtVtwq7Nt',
-  }),
-  url: 'https://b6vqk52mn0j3k6xj.svc01.us-south.eventstreams.cloud.ibm.com',
-  disableSslVerification: true,
-});
-
-const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
-
-const toneParams = {
-  toneInput: { 'text': text },
-  contentType: 'application/json',
+var toneInput = { "text": text };
+var params = {
+  'toneInput': toneInput,
+  'content_type': 'application/json'
 };
 
-  toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => {
-    console.log(JSON.stringify(toneAnalysis, null, 2));
+const tone_analyzer = new ToneAnalyzerV3({
+  version: 'YYYY-MM-DD',
+  authenticator: new IamAuthenticator({
+    apikey: 'xxxxxxxxxxxxxxx'
   })
-  .catch(err => {
-    console.log('error:', err);
-  });
-
-
-toneAnalyzer.method(params)
-.catch(err => {
-  console.log('error:', err);
 });
 
-const parameters = {parameters};
-          
-toneAnalyzer.methodName(
-  parameters,
-  headers = {
-    'Custom-Header': '{header_value}'
-  })
-   .then(result => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
-
- 
+tone_analyzer.tone(params, function(error, response) {
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    var tone = JSON.stringify(response, null, 2)
+    console.log("The tone analysis for \'" + text + "\' is:\n");
+    console.log(tone);
+  }
+});
--- src/routes/feedbacksRoute.js
+++ src/routes/feedbacksRoute.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const controller = require("../controllers/feedbacksController")
 
 router.get("/", controller.getTodos)
-router.get("/tipoProduto/:tipo", controller.getProduto) //REVER
 router.get("/:id", controller.getById)
 router.post("/", controller.post)
 router.put("/:id", controller.updateFeedback)
